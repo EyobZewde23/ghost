95b81d3f1fdb1489c51e6d885b26d921274451a0
Eyob Zewde <eyobdebrez@gmail.com>
2024-12-18 22:46:13 +0900

    firt commit

---
+++ Drum/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === "w") {
+      const sound = new Audio("dsound/w.MP3");
+      sound.play();
+      
+    }
+    else if (event.key==="a"){
+        const sound=new Audio("dsound/a.MP3")
+        sound.play()
+        
+    }
+    else if (event.key==="s"){
+        const sound=new Audio("dsound/s.MP3")
+        sound.play()
+       
+    }
+    else if (event.key==="d"){
+        const sound=new Audio("dsound/d.MP3")
+        sound.play()
+       
+    }
+    else if (event.key==="j"){
+        const sound=new Audio("dsound/j.MP3")
+        sound.play()
+        
+    }
+    else if (event.key==="k"){
+        const sound=new Audio("dsound/k.MP3")
+        sound.play()
+        
+    }
+    else if (event.key==="l"){
+        const sound=new Audio("dsound/l.MP3")
+        sound.play()
+       
+    }
+  });
+  
